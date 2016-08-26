7b0c3822015583d01d86e9b2c6082f24efde8d98
cjlee <cjlee@ngardensoft.com>
2016-08-26 12:46:13 +0900

    aaaaaaaaaaaaaaaaaaa
    Signed-off-by: cjlee <cjlee@ngardensoft.com>

---
+++ Assets/TestFolder/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		GUI.Button(new Rect(Screen.width * 0.35f, Screen.height * 0.45f, Screen.width * 0.3f, Screen.height * 0.1f), "Test Script");
+	}
+}
